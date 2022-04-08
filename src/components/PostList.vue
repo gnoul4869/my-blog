dfbbea5ee9f6d03ef3a1f15f200a71173461b1a7
gnoul_ <ttluong4869@gmail.com>
2022-04-08 20:20:49 +0700

    Create PostList

---
+++ src/components/PostList.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="post-list">
+        <div v-for="post in posts" :key="post.id">
+            <h2>{{ post.title }}</h2>
+            <p>{{ post.body }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['posts'],
+};
+</script>
+
+<style></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,42 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home">
+        <h1>Home</h1>
+        <div v-if="error">{{ error }}</div>
+        <div v-if="posts.length">
+            <PostList :posts="posts" />
+        </div>
+        <div v-else>
+            <p>Loading...</p>
+        </div>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import { ref } from '@vue/reactivity';
+import PostList from '../components/PostList.vue';
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+    name: 'HomeView',
+    components: { PostList },
+    setup() {
+        const posts = ref([]);
+        const error = ref(null);
+
+        const load = async () => {
+            try {
+                let data = await fetch('http://localhost:3000/posts');
+                if (!data.ok) {
+                    throw Error('No data available');
+                }
+                posts.value = await data.json();
+            } catch (err) {
+                error.value = err.message;
+                console.log(error.value);
+            }
+        };
+
+        load();
+
+        return { posts, error };
+    },
+};
 </script>
