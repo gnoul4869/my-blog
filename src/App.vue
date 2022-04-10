da040f53b9bf2997295e8ebbba19956d9be8a80f
gnoul_ <ttluong4869@gmail.com>
2022-04-11 00:41:18 +0700

    Create Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,16 @@
 <template>
+    <Navbar />
     <router-view />
 </template>
+
+<script>
+import Navbar from './components/Navbar.vue';
+
+export default {
+    name: 'App',
+    components: { Navbar },
+};
+</script>
 
 <style>
 #app {
