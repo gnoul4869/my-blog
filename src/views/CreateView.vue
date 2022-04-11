2ecccfb2e0f344cea35e89bb62d99b607c12277d
gnoul_ <ttluong4869@gmail.com>
2022-04-11 13:37:11 +0700

    Add return to home feature after creating new blog

--- src/views/CreateView.vue
+++ src/views/CreateView.vue
@@ -15,6 +15,7 @@
 
 <script>
 import { ref } from 'vue';
+import { useRouter } from 'vue-router';
 
 export default {
     setup() {
@@ -22,6 +23,8 @@ export default {
         const body = ref('');
         const tag = ref('');
         const tags = ref([]);
+
+        const router = useRouter();
 
         const handleKeydown = () => {
             tag.value = tag.value.replace(/\s/g, ''); // Remove all whitespace
@@ -50,6 +53,8 @@ export default {
                 title.value = '';
                 body.value = '';
                 tags.value = [];
+
+                router.push({ name: 'Home' });
             } catch (error) {
                 console.error(error);
             }
