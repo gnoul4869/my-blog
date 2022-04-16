873500c2d28ed8593b31e973da87976ed0eb52c0
gnoul_ <ttluong4869@gmail.com>
2022-04-16 19:05:17 +0700

    Create delete button to delete post

--- src/views/DetailsView.vue
+++ src/views/DetailsView.vue
@@ -4,6 +4,7 @@
         <h3>{{ post.title }}</h3>
         <p class="pre">{{ post.body }}</p>
         <span v-for="tag in post.tags" :key="tag"> #{{ tag }} </span>
+        <button @click="handleClick" class="delete">Delete</button>
     </div>
     <div v-else>
         <Spinner />
@@ -13,19 +14,31 @@
 <script>
 import getPost from '../composables/getPost';
 import Spinner from '../components/Spinner.vue';
-import { useRoute } from 'vue-router';
+import { useRoute, useRouter } from 'vue-router';
+import { doc, deleteDoc } from '@firebase/firestore';
+import { firestore } from '@/firebase/config';
 
 export default {
     components: { Spinner },
     props: ['id'],
     setup(props) {
         const route = useRoute();
+        const router = useRouter();
         const { id } = route.params;
         const { post, error, load } = getPost(id);
 
         load();
 
-        return { post, error };
+        const handleClick = async () => {
+            try {
+                await deleteDoc(doc(firestore, 'posts', id));
+                router.push({ name: 'Home' });
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        return { post, error, handleClick };
     },
 };
 </script>
@@ -42,5 +55,11 @@ export default {
 }
 .pre {
     white-space: pre-wrap;
+}
+
+button.delete {
+    margin-top: 40px;
+    font-size: 15px;
+    cursor: pointer;
 }
 </style>
